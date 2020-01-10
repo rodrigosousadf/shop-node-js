edf6a4ade9d2391769741078277bca7d106d8b24
Rodrigo <rodrigo.so82@hotmail.com>
2020-01-10 00:19:35 -0300

    Initial commit

---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+        const { products } = adminData;
+        res.render('shop', {
+                prods: products,
+                path: '/',
+                pageTitle: 'Shop',
+                hasProducts: products.length > 0,
+                activeShop: true,
+                productCss: true,
+        });
+});
+
+module.exports = router;
